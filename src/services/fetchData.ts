46902c4b0cbf38970cc1b67e967d2f3098d53f02
PHCavalcante <pedhcavdsan1603@gmail.com>
2024-12-20 22:25:23 -0300

    feat: adding postTask function in fetchData

--- src/services/fetchData.ts
+++ src/services/fetchData.ts
@@ -5,11 +5,19 @@ export async function fetchData(){
     try{
         const response = await axios.get("http://localhost:3000/tasks");
         return response.data;
-    } catch(error){
+    } catch (error){
         console.error(error);
     }
 }
-
+export async function postTask(newTask: taskTypes){
+    try{
+        const response = await axios.post("http://localhost:3000/tasks", newTask);
+        console.log(response);
+        return response.data;
+    } catch (error){
+        console.error(error);
+    }
+}
 export async function updateTask(task : taskTypes){
     try{
         const response = await axios.put(`http://localhost:3000/tasks/${task.id}`,
@@ -20,6 +28,14 @@ export async function updateTask(task : taskTypes){
             });
             console.log(response);
             return response.data;
+    } catch (error){
+        console.error(error);
+    }
+}
+export async function deleteTask(taskId : string){
+    try{
+        const response = await axios.delete(`http://localhost:3000/tasks/${taskId}`);
+        return response;
     } catch (error){
         console.error(error);
     }
