c0f69a7d8c79834d5ec4098e6fe3644885efa064
PHCavalcante <pedhcavdsan1603@gmail.com>
2025-01-05 22:53:46 -0300

    refactor: adding selection color in entire app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,12 @@ import Header from "./components/Header";
 
 export default function Home() {
   return (
-    <div className="flex gap-5 h-screen w-full">
+    <div className="flex gap-5 h-screen w-full selection:bg-[#ddd7d7]">
       <Menu />
       <div className="flex flex-col w-full h-full gap-4">
         <Header />
         <TaskManager />
-      </div>  
+      </div>
     </div>
   );
 }
