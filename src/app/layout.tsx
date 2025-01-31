6260690baaabe69359cc433ae05b74d8fa2af4a7
PHCavalcante <pedhcavdsan1603@gmail.com>
2025-01-31 13:15:00 -0300

    feat: ui improvements in root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,10 +6,11 @@ import {
 } from "@clerk/nextjs";
 import { TaskProvider } from "@/hooks/taskContext";
 import { Metadata } from "next";
+import Image from "next/image";
 import "./globals.css";
-
+import logo from "../assets/logo.svg";
 export const metadata: Metadata = {
-  title: "Todo-app",
+  title: "Tamarind",
   description: "description placeholder",
 };
 
@@ -24,7 +25,8 @@ export default function RootLayout({
         <html lang="en">
           <body className="flex h-screen">
             <SignedOut>
-              <div className="flex flex-col items-center justify-center w-full">
+              <div className="flex flex-col items-center justify-center w-full bg-gradient-to-tr from-[#F3EDED] via-rose-100 to-[#FFF9F9]">
+                <Image width={200} src={logo} alt="Tamarind Logo"/>
                 <SignIn routing="hash" />
               </div>
             </SignedOut>
