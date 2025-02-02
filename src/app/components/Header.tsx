fc81cf3d7dd99d75a0d512887c81a8d97b458818
PHCavalcante <pedhcavdsan1603@gmail.com>
2025-02-02 13:43:15 -0300

    refactor: updating Header component

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,15 +1,23 @@
+"use client";
+
+import GetUserData from "@/utils/GetUserData";
 import { UserButton } from "@clerk/nextjs";
+
 export default function Header(){
+    const user = GetUserData();
     const week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     const date = new Date();
     const weekDay = week[date.getDay()];
     return (
-        <header className="flex w-full h-16 justify-between bg-[#F3EDED] content-center py-4 px-3 rounded-2xl shadow-lg">
-            <h1 className="text-2xl font-bold pl-8">Tasks</h1> {/* Padding apenas para o icone do menu se encaixar no header quando o menu é ocultado */}
-            <div className="flex items-center gap-3">
-                <p className="font-semibold">{weekDay}</p>
-                <UserButton />
-            </div>
-        </header>
+      <header className="flex w-full h-16 justify-between items-center bg-[#F3EDED] content-center px-3 rounded-2xl shadow-lg">
+        <h1 className="text-xl">
+          Hello! {user && user.firstName}
+        </h1>
+        {/* Padding apenas para o icone do menu se encaixar no header quando o menu é ocultado */}
+        <div className="flex items-center gap-3">
+          <p className="font-light">{weekDay}</p>
+          <UserButton />
+        </div>
+      </header>
     );
 }
