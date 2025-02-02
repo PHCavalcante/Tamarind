a7c450f303685cdc9725c85bc320ca589a0bc856
PHCavalcante <pedhcavdsan1603@gmail.com>
2025-02-02 13:47:29 -0300

    feat: adding Kanban route option

--- src/app/components/TaskManager.tsx
+++ src/app/components/TaskManager.tsx
@@ -4,14 +4,17 @@ import { UseTaskContext } from "@/hooks/taskContext";
 import TaskMenu from "./TaskMenu";
 import DefaultTaskMenu from "./DefaultTaskMenu";
 import TaskMenuBlank from "./TaskMenuBlank";
+import Kanban from "./Kanban";
 
 export default function TaskManager() {
   const { selectedTask } = UseTaskContext();
-
+  console.log(selectedTask);
   return selectedTask == "List" ? (
     <TaskMenuBlank action="List" />
   ) : selectedTask == "Note" ? (
     <TaskMenuBlank action="Note" />
+  ) : selectedTask == "Kanban" ? (
+    <Kanban />
   ) : selectedTask ? (
     <TaskMenu />
   ) : (
