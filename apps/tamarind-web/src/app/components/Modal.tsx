11f7ce29b4b58f10be8d360230c5171d798ca82d
PHCavalcante <pedhcavdsan1603@gmail.com>
2025-03-16 00:02:03 -0300

    refacotr: making modal responsible

--- apps/tamarind-web/src/app/components/Modal.tsx
+++ apps/tamarind-web/src/app/components/Modal.tsx
@@ -11,6 +11,7 @@ import {
   deleteList,
 } from "@/services/fetchData";
 import createdAt from "@/utils/getFormattedDateTime";
+import { UseSnackbarContext } from "@/hooks/snackbarContext";
 
 type modalProps = {
   openModal: boolean;
@@ -29,6 +30,7 @@ export default function Modal({
   const { selectedTask, setSelectedTask } = UseTaskContext();
   const [value, setValue] = useState(false);
   const [descriptionLength, setDescriptionLength] = useState(0);
+  const { setOpenSnackbar, setSnackbarMessage } = UseSnackbarContext();
 
   const isTask = (task: unknown): task is taskTypes =>
     typeof task === "object" && task !== null && "title" in task;
@@ -40,7 +42,10 @@ export default function Modal({
     typeof note === "object" && note !== null && "description" in note;
 
   const formValues = useRef({
-    _id: isTask(selectedTask) ? selectedTask._id : "",
+    _id:
+      isTask(selectedTask) || isNote(selectedTask)
+        ? (selectedTask._id ?? undefined)
+        : undefined,
     title: "",
     description: "",
     scheduleDate: "",
@@ -53,11 +58,11 @@ export default function Modal({
   if (user) {
     formValues.current.userId = user.id ?? "";
   }
- 
+
   function handleAction() {
     if (action == "Delete" || action == "Finished") {
       return (
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center text-center">
           <h1>
             {action == "Delete"
               ? `Are you sure you want to delete this ${isTask(selectedTask) && selectedTask.type}?`
@@ -80,40 +85,48 @@ export default function Modal({
             </button>
             <button
               onClick={
-                (action == "Delete" && isTask(selectedTask) && selectedTask.type == "task" && !selectedTask.isCompleted)
+                action == "Delete" &&
+                isTask(selectedTask) &&
+                selectedTask.type == "task" &&
+                !selectedTask.isCompleted
                   ? () => {
-                      console.log("0")
                       deleteTask(selectedTask._id!);
                       setOpenModal(false);
                       setSelectedTask(null);
+                      setOpenSnackbar(true);
+                      setSnackbarMessage("Delete");
                     }
-                  : action == "Delete" && (isTask(selectedTask) && selectedTask.isCompleted)
-                  ? () => {
-                      console.log("1")
-                      setOpenModal(false);
-                      setSelectedTask(null);
-                    }
-                  : action == "Delete" && isNote(selectedTask)
-                  ? () => {
-                      console.log("2")
-                      deleteNote(selectedTask._id!);
-                      setSelectedTask(null);
-                    }
-                  : action == "Delete" && isList(selectedTask)
-                  ? () => {
-                      console.log("3")
-                      deleteList(selectedTask._id!);
-                      setSelectedTask(null);
-                    }
-                  : () => {
-                      console.log("4")
-                      if (isTask(selectedTask)) {
-                        selectedTask!.isCompleted = true;
+                  : action == "Delete" &&
+                      isTask(selectedTask) &&
+                      selectedTask.isCompleted
+                    ? () => {
+                        setOpenModal(false);
+                        setSelectedTask(null);
+                        setOpenSnackbar(true);
+                        setSnackbarMessage("Delete");
                       }
-                      updateTask(selectedTask as taskTypes);
-                      setOpenModal(false);
-                    }
-                }
+                    : action == "Delete" && isNote(selectedTask)
+                      ? () => {
+                          deleteNote(selectedTask._id!);
+                          setSelectedTask(null);
+                          setOpenSnackbar(true);
+                          setSnackbarMessage("DeleteNote");
+                        }
+                      : action == "Delete" && isList(selectedTask)
+                        ? () => {
+                            deleteList(selectedTask._id!);
+                            setSelectedTask(null);
+                            setOpenSnackbar(true);
+                            setSnackbarMessage("DeleteList");
+                          }
+                        : () => {
+                            if (isTask(selectedTask)) {
+                              selectedTask!.isCompleted = true;
+                            }
+                            updateTask(selectedTask as taskTypes);
+                            setOpenModal(false);
+                          }
+              }
               className="bg-[#FF5C5C] text-white font-bold w-24 py-2 px-2 rounded-lg mx-auto hover:scale-105"
             >
               {action == "Delete" ? "Delete" : "Yes!"}
@@ -129,7 +142,9 @@ export default function Modal({
             required
             type="text"
             placeholder={
-              action == "Edit" ? `New ${isTask(selectedTask) && selectedTask?.type} name` : "Task name"
+              action == "Edit"
+                ? `New ${isTask(selectedTask) && selectedTask?.type} name`
+                : "Task name"
             }
             autoFocus
             autoCapitalize="words"
@@ -137,14 +152,17 @@ export default function Modal({
             onChange={(e) => (formValues.current.title = e.target.value)}
           />
           <textarea
-            className="bg-white h-40 rounded-lg p-2 w-[800px] max-w-full"
+            className="bg-white min-h-40 rounded-lg p-2 max-h-[200px]"
             maxLength={500}
             placeholder={
               action == "Edit"
                 ? `New ${isTask(selectedTask) && selectedTask?.type} description`
                 : "Task description"
             }
-            onChange={(e) => {formValues.current.description = e.target.value; setDescriptionLength(e.target.value.length)}}
+            onChange={(e) => {
+              formValues.current.description = e.target.value;
+              setDescriptionLength(e.target.value.length);
+            }}
           />
           <span className="self-end">{descriptionLength}/500</span>
           <div className="flex items-center justify-between max-w-[70%]">
@@ -174,13 +192,12 @@ export default function Modal({
                       new Date(e.target.value).toLocaleDateString() +
                       " " +
                       new Date(e.target.value).toLocaleTimeString();
-                    console.log(formValues.current.scheduleDate);
                   }}
                 />
               </div>
             )}
           </div>
-          <div className="flex justify-between mt-3 w-[50%] mx-auto">
+          <div className="flex justify-between mt-3 md:w-[50%] md:mx-auto">
             <button
               className="text-[#FF5C5C] font-bold max-w-fit py-2 px-2 rounded-lg mx-auto hover:bg-[#FF5C5C22]"
               onClick={() => setOpenModal(false)}
@@ -188,18 +205,43 @@ export default function Modal({
               Cancel
             </button>
             <button
-              onClick={() =>
-                action == "Edit" && isTask(selectedTask) && selectedTask?.type == "task"
-                  ? updateTask({ ...formValues.current, type: "task" })
-                  : action == "Add new"
-                  ? postTask({ ...formValues.current, type: "task" })
-                  : action == "Edit" && isTask(selectedTask) && selectedTask?.type == "note"
-                  ? updateNote(formValues.current)
-                  : null
-              }
+              type="button"
+              onClick={(event) => {
+                event.preventDefault();
+                if (
+                  action == "Edit" &&
+                  isTask(selectedTask) &&
+                  selectedTask?.type == "task"
+                ) {
+                  updateTask({
+                    ...formValues.current,
+                    type: "task",
+                    _id: formValues.current._id ?? undefined,
+                  });
+                  setOpenModal(false);
+                  setOpenSnackbar(true);
+                  setSnackbarMessage("Update");
+                } else if (action == "Add new") {
+                  postTask({ ...formValues.current, type: "task" });
+                  setOpenModal(false);
+                  setOpenSnackbar(true);
+                  setSnackbarMessage("Add");
+                } else if (
+                  action == "Edit" &&
+                  isTask(selectedTask) &&
+                  selectedTask?.type == "note"
+                ) {
+                  updateNote(formValues.current);
+                  setOpenSnackbar(true);
+                  setSnackbarMessage("UpdateNote");
+                }
+              }}
               className="bg-[#201335bb] font-bold text-[#fff] max-w-fit py-2 px-2 rounded-lg mx-auto hover:bg-[#201335dd]"
             >
-              {action} {isTask(selectedTask) && selectedTask?.type == "note" ? "note" : "task"}
+              {action}{" "}
+              {isTask(selectedTask) && selectedTask?.type == "note"
+                ? "note"
+                : "task"}
             </button>
           </div>
         </form>
@@ -216,18 +258,19 @@ export default function Modal({
     >
       <div
         onClick={(e) => e.stopPropagation()}
-        className="flex flex-col bg-[#F3EDED] w-fit max-w-[50%] mx-auto rounded-xl py-5 px-5 transform transition-all duration-300 animate-modal"
+        className={`flex flex-col max-h-full bg-[#F3EDED] min-w-fit ${action == "Delete" || action == "Finished" ? "w-fit" : "max-w-[50%]"} mx-auto rounded-xl py-5 px-5 transform transition-all duration-300 animate-modal`}
       >
         <div className="flex justify-between mb-5">
           <h2 className="font-bold text-2xl mx-auto">
             {action}{" "}
-            {isTask(selectedTask) && (selectedTask?.type == "note"
-              ? "note"
-              : selectedTask?.type == "task"
-              ? "task"
-              : selectedTask?.type == "list"
-                ? "list"
-              : "task")}
+            {isTask(selectedTask) &&
+              (selectedTask?.type == "note"
+                ? "note"
+                : selectedTask?.type == "task"
+                  ? "task"
+                  : selectedTask?.type == "list"
+                    ? "list"
+                    : "task")}
           </h2>
         </div>
         {handleAction()}
