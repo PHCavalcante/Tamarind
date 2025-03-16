58a3a283999f6afced8e3ea2d90868c5d2de2446
PHCavalcante <pedhcavdsan1603@gmail.com>
2025-03-15 21:51:56 -0300

    feat: adding favicon in layout.tsx

--- apps/tamarind-web/src/app/layout.tsx
+++ apps/tamarind-web/src/app/layout.tsx
@@ -9,9 +9,14 @@ import { Metadata } from "next";
 import Image from "next/image";
 import "./globals.css";
 import logo from "../assets/logo.svg";
+import { SnackProvider } from "@/hooks/snackbarContext";
 export const metadata: Metadata = {
   title: "Tamarind",
   description: "description placeholder",
+  icons: {
+    icon: "./favicon.ico",
+    apple: "./apple-icon.png"
+  }
 };
 
 export default function RootLayout({
@@ -22,17 +27,19 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <TaskProvider>
-        <html lang="en">
-          <body className="flex h-screen">
-            <SignedOut>
-              <div className="flex flex-col items-center justify-center w-full bg-gradient-to-tr from-[#F3EDED] via-rose-100 to-[#FFF9F9]">
-                <Image width={200} src={logo} alt="Tamarind Logo"/>
-                <SignIn routing="hash" />
-              </div>
-            </SignedOut>
-            <SignedIn>{children}</SignedIn>
-          </body>
-        </html>
+        <SnackProvider>
+          <html lang="en">
+            <body className="flex h-screen">
+              <SignedOut>
+                <div className="flex flex-col items-center justify-center w-full bg-gradient-to-tr from-[#F3EDED] via-rose-100 to-[#FFF9F9]">
+                  <Image width={200} src={logo} alt="Tamarind Logo" />
+                  <SignIn routing="hash" />
+                </div>
+              </SignedOut>
+              <SignedIn>{children}</SignedIn>
+            </body>
+          </html>
+        </SnackProvider>
       </TaskProvider>
     </ClerkProvider>
   );
